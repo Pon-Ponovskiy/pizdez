67faac418cae11132c3bde35bff0ff91e5e2f2cf
Pon-Ponovskiy <celoveka269@gmail.com>
2026-02-15 20:01:58 +0300

    df

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-document.addEventListener('DOMContentLoaded', ready)
+document.addEventListener('DOMContentLoaded', function(){
+    let search = document.querySelector('.header__search')// сама кнопка
+    let textField = document.querySelector('.header__text-field')//ввод текста
+    
+    search.addEventListener('click', ()=>{
+        textField.classList.toggle('active')
+    })
+
+    let header = document.querySelector('.header')
+    let widthScreen = document.documentElement.clientWidth 
+    let headerHeight = header.clientHeight
+
+    if(widthScreen > 1024){
+        window.onscroll = changeHeader
+        let lastScroll = 0
+        function changeHeader(){
+            const scrollTop = document.documentElement.scrollTop
+            if(lastScroll > scrollTop){
+                header.style.top = `0px`
+            }
+            else{
+                if(window.pageYOffset > 200){
+                    header.style.top = `-${headerHeight}px`
+                }
+            }
+            lastScroll = scrollTop;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})  
